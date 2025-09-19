2541f1d65d06bbec3d05bb25fc4dc9aa214dfec7
kabozek1 <e.cerf2015@gmail.com>
2025-09-19 19:18:55 +0900

    Delete tgbot_final02 directory

--- tgbot_final02/src/plugins/admin_panel/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Упрощенная админ панель для Telegram бота
-"""
-
-from .main import register
-
-__all__ = ['register']
--- tgbot_final02/src/plugins/post_manager_plugin.py
+++
@@ -1,8 +0,0 @@
-import logging
-from aiogram import Dispatcher
-from sqlalchemy.ext.asyncio import async_sessionmaker
-
-logger = logging.getLogger(__name__)
-
-def register(dp: Dispatcher, bot, async_session_local: async_sessionmaker):
-    logger.info("✅ Registered post_manager_plugin (placeholder).")
